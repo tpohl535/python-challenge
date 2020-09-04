7ee2f34c769055b114faccb57d1b401e50212010
Tyler Pohl <tyler.m.pohl@gmail.com>
2020-09-04 06:13:54 +0000

    opened csv

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,12 @@
 import os
+import csv
+
+IN_PATH = os.path.join("Resources", "election_data.csv")
+OUT_PATH = os.path.join("Analysis", "election_data_analysis.txt")
+
+with open(IN_PATH, "r") as in_file, open(OUT_PATH, "w+") as out_file:
+    
+    reader = csv.DictReader(in_file)
+    
+    data = list(reader)
+    
