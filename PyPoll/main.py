f48844d192076976b3adf8389a81db717ef25c1b
Tyler Pohl <tyler.m.pohl@gmail.com>
2020-09-04 10:32:48 +0000

    prepared code to import subset

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,34 @@
 import os
 import csv
 
-IN_PATH = os.path.join("Resources", "election_data.csv")
+IN_PATH = os.path.join("election_data_subsample.cvs")
 OUT_PATH = os.path.join("Analysis", "election_data_analysis.txt")
 
-with open(IN_PATH, "r") as in_file, open(OUT_PATH, "w+") as out_file:
+with open(IN_PATH, "r") as in_file, open(OUT_PATH, "w+") as out_file:  
     
-    reader = csv.DictReader(in_file)
+    reader = csv.reader(in_file)
+    header = next(reader)
     
-    data = list(reader)
+
+    count = 0
+    
+    for row in reader:
+        
+        count += 1
+        
+        
+        
+        
+        
+    # format print to example   
+    output_txt = f"Election Results\n"\
+    f"-------------------------\n"\
+    f"Total Votes: {count}\n"\
+    f"-------------------------\n"\
+    f"Khan: \n"\
+    f"Corry: \n"\
+    f"Li: \n"\
+    f"O'Tooley: \n"\
+    
+    print(count)
     
