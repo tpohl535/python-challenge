202623a0001191610c14b0b467c3043f3cdbbd6e
Tyler Pohl <tyler.m.pohl@gmail.com>
2020-09-05 22:54:03 +0000

    finished project

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-IN_PATH = os.path.join("subset.csv")
+IN_PATH = os.path.join("Resources", "election_data.csv")
 OUT_PATH = os.path.join("Analysis", "election_data_analysis.txt")
 
 
@@ -12,67 +12,50 @@ with open(IN_PATH, 'r') as in_file:
     header = next(reader)
     
     retrieve_cand = []
-    khan = []
-    corry = []
-    li = []
-    otooley = []
-    
+    total_votes = 0
+
     for row in reader:
         retrieve_cand.append(row[2])
         
-    cand_name_count = []
-    cand_name_count = dict(name)
+    cand_name_count = dict((name, retrieve_cand.count(name)) for name in set(retrieve_cand)) 
+    
+    v = list(cand_name_count.values())
+    k = list(cand_name_count.keys())
+    winner = k[v.index(max(v))] 
+    
+       
+    for x, y in cand_name_count.items():
         
-    for x , y in retrieve_cand:
-        if 
+        total_votes += y
         
-    for row in retrieve_cand:
-        if row == "Khan":
-            khan.append(row)
-            k_vote = len(khan)
-        elif row == "Correy":
-            corry.append(row)
-            c_vote = len(corry)
-        elif row == "Li":
-            li.append(row)
-            l_vote = len(li)
+        if x == "Khan":
+            k_vote = y
+            average_k = round((y / total_votes), 2)
+        elif x == "Correy":
+            c_vote = y
+        elif x == "Li":
+            l_vote = y
         else:
-            otooley.append(row)
-            o_vote = len(otooley)
-     
-    total_vote = k_vote + c_vote + l_vote + o_vote
-    average_k = round((k_vote / total_vote), 2) 
-    average_c = round((c_vote / total_vote), 2)
-    average_l = round((l_vote / total_vote), 2)
-    average_o = round((o_vote / total_vote), 2)
+            o_vote = y
         
-    
-
-print(total_vote)
-print(average_c)
-print(average_k)
-print(average_l)
-print(average_o)
-
-   
-data = [
-["Edward", 123],
-["Doug", 123123],
-["Edward", 2432],
-["Edward", 2432],
-["Umair", 345],
-["Edward", 2432]
-]
-results = {}
-for candidate, votes in data:
-    if candidate in results:
-        results[candidate] += votes
-    else:
-        results[candidate] = votes
-    
-    print(50  * "=")
-    print(results)
-print(50  * "=")
-print(results)        
-     
+    average_k = round((k_vote / total_votes)*100, 2)
+    average_c = round((c_vote / total_votes)*100, 2)
+    average_l = round((l_vote / total_votes)*100, 2)
+    average_o = round((o_vote / total_votes)*100, 2)
         
+output_text = f"Election Results \n"\
+f"---------------------------------------------------\n"\
+f"Total Votes: {total_votes}\n"\
+f"---------------------------------------------------\n"\
+f"Precent votes for Khan: {average_k}% ({k_vote}) \n"\
+f"Precent votes for Correy: {average_c}% ({c_vote})\n"\
+f"Precent votes for Li: {average_l}% ({l_vote})\n"\
+f"Precent votes for O'Tooley: {average_o}% ({o_vote})\n"\
+f"---------------------------------------------------\n"\
+f"Election Winner: {winner}\n"\
+f"---------------------------------------------------\n"\
+
+print(output_text)
+with open(OUT_PATH, "+w") as out_file:
+    out_file.write(output_text)
+ 
